76a8d35862cc1392ec72094b7dea102f54c7c520
tsy <tsyios@163.com>
2016-10-21 16:23:26 +0800

    优化

--- SYAttributedString/SYAttributedString/ViewController.m
+++ SYAttributedString/SYAttributedString/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "SYAttributedStringModel.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -17,6 +19,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *tempArray = [NSMutableArray array];
+    for (int i = 0; i < 5; i++) {
+        SYAttributedStringModel *model = [SYAttributedStringModel attributedStringModelWithFont:[UIFont systemFontOfSize:15] Color:@[[UIColor redColor],[UIColor blueColor],[UIColor purpleColor],[UIColor grayColor],[UIColor blackColor]][i] String:[NSString stringWithFormat:@"说什么呢+%d",i]];
+        [tempArray addObject:model];
+    }
+    NSAttributedString *attStr = [SYAttributedStringModel sy_attributedStringWithModels:tempArray];
+    self.label.attributedText = attStr;
+    NSRange attStrRange = NSMakeRange(0, attStr.length);
+    NSDictionary *dict = [attStr attributesAtIndex:0 effectiveRange:&attStrRange];
+    NSLog(@"===%@",dict);
 }
 
 
