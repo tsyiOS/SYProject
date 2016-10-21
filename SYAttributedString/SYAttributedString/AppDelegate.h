a162f2e288b21b4a54deaa44d40972e080bfc414
tsy <tsyios@163.com>
2016-10-21 10:54:21 +0800

    优化

---
+++ SYAttributedString/SYAttributedString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYAttributedString
+//
+//  Created by leju_esf on 16/10/21.
+//  Copyright © 2016年 tsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
