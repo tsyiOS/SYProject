8dacc5a8210dd58a41a41f68f54b489734784bc0
tsy <tsyios@163.com>
2016-10-27 10:26:07 +0800

    创建develop

---
+++ SYImagePickerDevelop/SYImagePickerDevelop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYImagePickerDevelop
+//
+//  Created by leju_esf on 16/10/27.
+//  Copyright © 2016年 tsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
