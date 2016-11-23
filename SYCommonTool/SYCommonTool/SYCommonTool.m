5f5001632562b0982f08c3d05f91e3d7477b13ad
tsy <tsyios@163.com>
2016-11-23 19:55:18 +0800

    手机验证

--- SYCommonTool/SYCommonTool/SYCommonTool.m
+++ SYCommonTool/SYCommonTool/SYCommonTool.m
@@ -62,16 +62,37 @@ static SYCommonTool *instance;
  */
 + (BOOL)sy_checkMobileNumber:(NSString *)mobileNum {
     
-    //    电信号段:133/153/180/181/189/177
-    //    联通号段:130/131/132/155/156/185/186/145/176
-    //    移动号段:134/135/136/137/138/139/150/151/152/157/158/159/182/183/184/187/188/147/178
-    //    虚拟运营商:170
-    
-    NSString *MOBILE = @"^1(3[0-9]|4[57]|5[012356789]|8[0-9]|7[0-9])\\d{8}$";
-    
-    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
-    
-    return [regextestmobile evaluateWithObject:mobileNum];
+    mobile = [mobile stringByReplacingOccurrencesOfString:@" " withString:@""];
+    if (mobile.length != 11)
+    {
+        return NO;
+    }else{
+        /**
+         * 移动号段正则表达式
+         */
+        NSString *CM_NUM = @"^((13[4-9])|(147)|(15[0-2,7-9])|(178)|(18[2-4,7-8]))\\d{8}|(1705)\\d{7}$";
+        /**
+         * 联通号段正则表达式
+         */
+        NSString *CU_NUM = @"^((13[0-2])|(145)|(15[5-6])|(176)|(18[5,6]))\\d{8}|(1709)\\d{7}$";
+        /**
+         * 电信号段正则表达式
+         */
+        NSString *CT_NUM = @"^((133)|(153)|(177)|(18[0,1,9]))\\d{8}$";
+        NSPredicate *pred1 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM_NUM];
+        BOOL isMatch1 = [pred1 evaluateWithObject:mobile];
+        NSPredicate *pred2 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU_NUM];
+        BOOL isMatch2 = [pred2 evaluateWithObject:mobile];
+        NSPredicate *pred3 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT_NUM];
+        BOOL isMatch3 = [pred3 evaluateWithObject:mobile];
+        
+        if (isMatch1 || isMatch2 || isMatch3) {
+            return YES;
+        }else{
+            return NO;
+        }
+    }
+
 }
 
 /**
